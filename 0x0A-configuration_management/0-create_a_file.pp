e97e4965e5960889925828cd5311c020b795cccd
esehtony <esehtony123@gmail.com>
2022-12-14 03:24:32 -0800

    correction to task 0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # creating a file
 file { 'school':
-path    => '/tmp',
-content => 'I love Puppet',
-owner   =>'www-data',
-group   => 'www-data',
-mode    => '0744'
+  path    => '/tmp/school',
+  content => 'I love Puppet',
+  owner   =>'www-data',
+  group   => 'www-data',
+  mode    => '0744'
 }
