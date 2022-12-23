3041009407b1c42331a5729fce69a46251140bda
tony <tony@computer.com>
2022-12-23 02:41:56 +0100

    solution to task 2 0x0F

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,31 @@
+# Configure Nginx server with pouppet
+
+package { 'nginx':
+  ensure => installed,
+}
+
+file { '/var/www/html/index.html':
+  content => 'Hello World!',
+}
+
+exec { 'add block server':
+  path    => '/usr/bin',
+  command => 'echo "server {
+        listen 80 default_server;
+        listen [::]:80 default_server;
+
+        root /var/www/html;
+        index index.html index.htm index.nginx-debian.html;
+
+	add_header X-Served-By $HOSTNAME;
+
+        location /redirect_me {
+                return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+        }
+}" | sudo tee /etc/nginx/sites-available/default'
+}
+
+exec {'restarting nginx':
+  path    => '/usr/bin',
+  command => 'sudo service nginx restart'
+}
