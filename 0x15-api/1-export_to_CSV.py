fa9916a8ec91860a05d8d0e93f0687ee287c1ac9
tony <tony@computer.com>
2023-01-16 21:52:53 +0100

    solution to task 1 0x15

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+returns information for a given employee ID about his/her TODO list progress
+"""
+from sys import argv
+import requests
+import json
+import csv
+
+
+if __name__ == "__main__":
+    user_data = requests.get(
+        "https://jsonplaceholder.typicode.com/users/{}".format(argv[1]))
+    user_data = json.loads(user_data.text)
+    user_todos = requests.get(
+        "https://jsonplaceholder.typicode.com/todos/?userId={}"
+        .format(user_data.get("id")))
+    user_todos = json.loads(user_todos.text)
+
+    user_dict = []
+    for todo in user_todos:
+        dic = {}
+        dic["id"] = todo["userId"]
+        dic["name"] = user_data["username"]
+        dic["status"] = todo["completed"]
+        dic["title"] = todo["title"]
+        user_dict.append(dic)
+
+    with open(f"{argv[1]}.csv", "w", newline="") as f:
+        if user_dict is None or user_dict == []:
+            f.write("[]")
+        else:
+            fieldname = ["id", "name", "status", "title"]
+            writer = csv.DictWriter(f, fieldnames=fieldname)
+            for todo in user_dict:
+                writer.writerow(todo)
