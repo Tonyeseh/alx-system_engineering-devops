64b97b9d4b84b9fff2ab3d309b69a48e0738543e
tony <tony@computer.com>
2023-01-16 21:10:38 +0100

    solution to task 0 0x15

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,6 +6,7 @@ from sys import argv
 import requests
 import json
 
+
 def get_completed_tasks(todos):
     """ gets the number of completed todo of a user"""
     completed = 0
@@ -15,12 +16,25 @@ def get_completed_tasks(todos):
 
     return completed
 
-if __name__ == "__main__":
-    user_data = requests.get("https://jsonplaceholder.typicode.com/users/{}".format(argv[1]))
-    user_data = json.loads(user_data.text)
-    user_todos = requests.get("https://jsonplaceholder.typicode.com/todos/?userId={}".format(user_data["id"]))
-    user_todos = json.loads(user_todos.text)
-    print("Employee {} is done with tasks({}/{}):".format(user_data["name"], get_completed_tasks(user_todos), len(user_todos)))
 
-#    print(user_todos)
-#    print(user_data)
+def print_tasks(todos):
+    """ prints the each tasks and appends a tab before it """
+    for todo in todos:
+        if todo["completed"]:
+            print("\t {}".format(todo["title"]))
+
+
+if __name__ == "__main__":
+    user_data = requests.get(
+        "https://jsonplaceholder.typicode.com/users/{}".format(argv[1]))
+    user_data = json.loads(user_data.text)
+    user_todos = requests.get(
+        "https://jsonplaceholder.typicode.com/todos/?userId={}"
+        .format(user_data["id"]))
+    user_todos = json.loads(user_todos.text)
+    print(
+        "Employee {} is done with tasks({}/{}):"
+        .format(user_data["name"],
+                get_completed_tasks(user_todos),
+                len(user_todos)))
+    print_tasks(user_todos)
