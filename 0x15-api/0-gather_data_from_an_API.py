cf609d552222d0e40573d295026cca0a6c37d078
tony <tony@computer.com>
2023-01-16 17:26:09 +0100

    started solution to task 0 0x15

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+returns information for a given employee ID about his/her TODO list progress
+"""
+from sys import argv
+import requests
+import json
+
+def get_completed_tasks(todos):
+    """ gets the number of completed todo of a user"""
+    completed = 0
+    for todo in todos:
+        if todo["completed"]:
+            completed += 1
+
+    return completed
+
+if __name__ == "__main__":
+    user_data = requests.get("https://jsonplaceholder.typicode.com/users/{}".format(argv[1]))
+    user_data = json.loads(user_data.text)
+    user_todos = requests.get("https://jsonplaceholder.typicode.com/todos/?userId={}".format(user_data["id"]))
+    user_todos = json.loads(user_todos.text)
+    print("Employee {} is done with tasks({}/{}):".format(user_data["name"], get_completed_tasks(user_todos), len(user_todos)))
+
+#    print(user_todos)
+#    print(user_data)
