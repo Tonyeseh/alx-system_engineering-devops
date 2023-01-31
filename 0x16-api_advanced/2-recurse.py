c222ef76170b507b68e88f49cdb1c5ff24a7bb5a
tony <tony@computer.com>
2023-01-31 21:41:27 +0100

    solution to task 2 0x16

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+Queries the reddit api and returns a list containing
+the titles of all hot articles for a given subreddit
+"""
+import requests
+
+
+def recurse(subreddit, hot_list=[], after=""):
+    """returnd a list containing the titles of all hot articles for a given subreddit.
+
+        Args:
+            subreddit(str): subreddit name
+            hot_list(list): list of hot articles titles
+            after:(str):
+    """
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {
+        "User-Agent": "alx_advanced api"
+    }
+    params = {
+        "after": after,
+        "limit": 100
+    }
+    res = requests.get(url, headers=headers, params=params, allow_redirects=False)
+    if res.status_code == 404:
+        return None
+    results = res.json().get('data')
+    after = results.get('after')
+    for post in results.get('children'):
+        hot_list.append(post.get('data').get('title'))
+
+    if after is not None:
+        return recurse(subreddit, hot_list, after)
+    return hot_list
