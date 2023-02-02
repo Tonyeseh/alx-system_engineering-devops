58849283d3e7e16489067cb9c6fde2a45710ffbc
tony <tony@computer.com>
2023-02-02 15:04:32 +0100

    fixed puppet-lint style error

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # fixes a wordpress site
 
 exec { 'fix-wordpress':
-	command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-	path => '/usr/local/bin/:/bin/'
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
 }
